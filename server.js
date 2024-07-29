99ea79df60febf61c8c4c45008111608dcf39c21
Alexander Grigoriev <closegamer@mail.ru>
2024-07-29 16:04:15 +0300

    sfsg

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ nextApp.prepare().then(async() => {
     const server = http.createServer(app);
     const io = require("socket.io")(server);
 
-    app.use(cors())
+    io.origins(['https://drtslrdsrv.ru:443']);
 
     // origin: ["http://localhost:3000", "http://localhost:3001", "http://dartslord.ru", "https://dartslord.ru", "http://dartslord.ru/socket.io/", "https://dartslord.ru/socket.io/", "wss://dartslord.ru/socket.io/", "ws://dartslord.ru/socket.io/", "wss://185.104.248.121:3001/socket.io/", "ws://185.104.248.121:3001/socket.io/", "http://185.104.248.121", "https://185.104.248.121", "wss://185.104.248.121:3001", "ws://185.104.248.121:3001"],
     io.attach(server);

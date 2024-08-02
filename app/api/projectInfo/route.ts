11252a74957456dd6244faedb2ff0f67369284fd
Alexander Grigoriev <closegamer@mail.ru>
2024-08-02 17:24:42 +0300

    asc

--- app/api/projectInfo/route.ts
+++ app/api/projectInfo/route.ts
@@ -102,7 +102,7 @@ export async function GET(request: Request) {
             },
             orderBy: [
                 {
-                    id: 'desc',
+                    id: 'asc',
                 }
             ],
         })
